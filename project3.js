5a7b693bfbe50f3db7991257f2d75e02d9d00b75
Nikhil Kushwaha <124434514+NIKHILKUSHWAHA9877@users.noreply.github.com>
2023-11-11 02:47:53 +0530

    Delete project3.js

--- project3.js
+++
@@ -1,7 +0,0 @@
-const contentcoaintener = document.querySelector('content-coaintener')
-;
-// for(i=0 ; i<contentcoaintener.length ; i++){
-contentcoaintener.addEventListener('click', function() {
-this.classList.toggle('active');
- });
-// };
